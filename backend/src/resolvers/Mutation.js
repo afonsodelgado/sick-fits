894b69af0afd1235a8d274b7fcb813794194f9cd
Afonso Delgado <afonsodelgadoss@gmail.com>
2018-10-31 21:05:48 -0300

    Add mutation resolvers

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,6 +2,17 @@ const Mutations = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem({ data: { ... args } }, info);
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args }
+    delete updates.id
+
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info)
   }
 };
 
