e36dd9b2e368586e0c926dd3aa8a117e1d12012b
Afonso Delgado <afonsodelgadoss@gmail.com>
2018-11-02 10:35:12 -0300

    Code cleanup

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -37,7 +37,11 @@ class Items extends Component {
     return (
       <Center>
         <Pagination page={this.props.page} />
-        <Query query={ALL_ITEMS_QUERY} variables={{ skip: this.props.page * perPage - perPage }}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{ skip: this.props.page * perPage - perPage }}
+          // fetchPolicy="network-only"
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error: {error.message}</p>
