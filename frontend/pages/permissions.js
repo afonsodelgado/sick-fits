86f7025c9affeb5758d56ff74f94343906a3e8fc
Afonso Delgado <afonsodelgadoss@gmail.com>
2018-11-07 11:10:29 -0200

    Add a Manage Permissions page

---
+++ frontend/pages/permissions.js
@@ -0,0 +1,12 @@
+import PleaseSignin from '../components/PelaseSignin'
+import Permissions from '../components/Permissions'
+
+const PermissionsPage = props => (
+  <div>
+    <PleaseSignin>
+      <Permissions />
+    </PleaseSignin>
+  </div>
+)
+
+export default PermissionsPage
