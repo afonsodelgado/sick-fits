1468657c80a5435644f8dbc3a9c6833dcee187c1
Afonso Delgado <afonsodelgadoss@gmail.com>
2018-11-09 10:06:10 -0200

    Add addToCart mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -144,6 +144,29 @@ const Mutations = {
       data: { permissions: { set: args.permissions } },
       where: { id: args.userId }
     }, info)
+  },
+  async addToCart(parent, args, ctx, info) {
+    const { userId } = ctx.request
+    if (!userId) throw new Error ('You must be signed in')
+
+    const [ existingCartItem ] = await ctx.db.query.cartItems({
+      where: {
+        user: { id: userId },
+        item: { id: args.id }
+      }
+    })
+
+    if (existingCartItem) return ctx.db.mutation.updateCartItem({
+      where: { id: existingCartItem.id },
+      data: { quantity: existingCartItem.quantity + 1 }
+    }, info)
+
+    return ctx.db.mutation.createCartItem({
+      data: {
+        user: { connect: { id: userId } },
+        item: { connect: { id: args.id } }
+      }
+    }, info)
   }
 };
 
