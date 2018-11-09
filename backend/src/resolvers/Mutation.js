7f3dab15d1299f7673a720d681a487070fb501a2
Afonso Delgado <afonsodelgadoss@gmail.com>
2018-11-09 13:43:20 -0200

    Add removeFromCart mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -167,6 +167,20 @@ const Mutations = {
         item: { connect: { id: args.id } }
       }
     }, info)
+  },
+  async removeFromCart(parent, args, ctx, info) {
+    const cartItem = await ctx.db.query.cartItem({
+      where: {
+        id: args.id
+      }
+    }, `{ id user { id } }`)
+
+    if (!cartItem) throw new Error('No item found')
+    if (cartItem.user.id !== ctx.request.userId) throw new Error('This item is not yours')
+
+    return ctx.db.mutation.deleteCartItem({
+      where: { id: args.id }
+    }, info)
   }
 };
 
