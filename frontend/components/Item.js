49945f07f459e666c14fda2b9b24ad91f3834844
Afonso Delgado <afonsodelgadoss@gmail.com>
2018-11-09 10:06:25 -0200

    Add AddToCart component to Item

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
-import Title from './styles/Title';
-import ItemStyles from './styles/ItemStyles';
-import PriceTag from './styles/PriceTag';
+import Title from './styles/Title'
+import ItemStyles from './styles/ItemStyles'
+import PriceTag from './styles/PriceTag'
 import DeleteItem from './DeleteItem'
+import AddToCart from './AddToCart'
 
 import formatMoney from '../lib/formatMoney';
 
@@ -40,7 +41,7 @@ export default class Item extends Component {
           }}>
             <a>Edit</a>
           </Link>
-          <button>Add To Cart</button>
+          <AddToCart id={item.id}/>
           <DeleteItem id={item.id}>Delete Item</DeleteItem>
         </div>
       </ItemStyles>
