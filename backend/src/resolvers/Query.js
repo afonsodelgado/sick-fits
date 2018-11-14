11f1a7f2ecc41eb4e94c601fd3bb13d20443bb36
Afonso Delgado <afonsodelgadoss@gmail.com>
2018-11-14 08:34:17 -0200

    Add order query to fetch a single order information

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -20,6 +20,20 @@ const Query = {
     // check if user have permission to query users
     hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
     return ctx.db.query.users({}, info)
+  },
+  async order(parent, args, ctx, info) {
+    if (!ctx.request.userId) throw new Error('You nedd to log in')
+
+    const order = await ctx.db.query.order({
+      where: { id: args.id }
+    }, info)
+
+    const ownsOrder = order.user.id === ctx.request.userId
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN')
+
+    if (!ownsOrder || !hasPermissionToSeeOrder) throw new Error('You cant see this')
+
+    return order
   }
 };
 
