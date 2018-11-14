9fd744782d760f7cc15385c3a22fe2597b34fa17
Afonso Delgado <afonsodelgadoss@gmail.com>
2018-11-14 08:34:34 -0200

    Route user to order page when the checkout is done

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -29,6 +29,8 @@ function totalItems(cart) {
 
 class TakeMyMoney extends React.Component {
   onToken = async (res, createOrder) => {
+    NProgress.start()
+    
     //the res.id is the stripe token for charging
     const order = await createOrder({
       variables: {
@@ -36,7 +38,10 @@ class TakeMyMoney extends React.Component {
       }
     }).catch(err => alert(err.message))
 
-    console.log(order)
+    Router.push({
+      pathname: '/order',
+      query: { id: order.data.createOrder.id }
+    })
   }
 
   render() {
